9810bfd92c35fcfa9330f1f9ed72f8e643c56271
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-08-16 07:31:03 +0200

    Create and style Login component

---
+++ src/components/App/App.js
@@ -0,0 +1,25 @@
+/**
+ * Import
+ */
+import React from "react";
+
+/**
+ * Local import
+ */
+// Composants
+import Login from "../Login";
+// Styles et assets
+import "./app.scss";
+
+/**
+ * Code
+ */
+const App = () => (
+  <Login />
+  // {/* <LeafletMap /> */}
+);
+
+/**
+ * Export
+ */
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App/App';
+import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/index.sass";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
